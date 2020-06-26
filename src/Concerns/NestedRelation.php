6a898e25220127c7a678b9e876c9f9e8320287d4
zingimmick <zingimmick@outlook.com>
2020-06-27 01:55:22 +0800

    Fix #9

---
+++ src/Concerns/NestedRelation.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Zing\QueryBuilder\Concerns;
+
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
+
+trait NestedRelation
+{
+    protected function resolveNestedRelation($property)
+    {
+        return collect(explode('.', $property))
+            ->pipe(
+                function (Collection $parts) {
+                    return [
+                        $parts->except(count($parts) - 1)->map([Str::class, 'camel'])->implode('.'),
+                        $parts->last(),
+                    ];
+                }
+            );
+    }
+}
