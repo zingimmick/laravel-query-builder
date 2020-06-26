2405806d162bee46a5a26f29b63e934a64ae5c04
zingimmick <zingimmick@outlook.com>
2020-06-27 01:42:07 +0800

    Fix #8

--- src/Concerns/WithSearchable.php
+++ src/Concerns/WithSearchable.php
@@ -14,42 +14,46 @@ trait WithSearchable
     {
         $searchable = is_array($searchable) ? $searchable : func_get_args();
         $search = $this->request->input('search');
-        if ($search) {
-            $this->where(
-                function (Builder $query) use ($search, $searchable): void {
-                    $results = [];
-                    foreach ($searchable as $field) {
-                        if (Str::contains($field, '.')) {
-                            $results = $this->addNestedRelation($field, $results);
-                        } else {
-                            $results[] = $field;
-                        }
-                    }
+        if ($search === null || (is_string($search) && trim($search) === '')) {
+            return $this;
+        }
 
-                    foreach ($results as $key => $value) {
+        $searchable = $this->resolveNestedSearchable($searchable);
+
+        return $this->applySearchable($search, $searchable);
+    }
+
+    protected function applySearchable($search, array $searchable = [])
+    {
+        return $this->where(
+            function (Builder $query) use ($search, $searchable): void {
+                collect($searchable)->each(
+                    function ($value, $key) use ($query, $search) {
                         if (is_numeric($key)) {
-                            $query->orWhere($value, 'like', "%{$search}%");
-                        } else {
-                            $query->orWhereHas(
-                                $key,
-                                function ($query) use ($value, $search): void {
-                                    /** @var \Illuminate\Database\Query\Builder $query */
-                                    $query->where(
-                                        function (Builder $query) use ($value, $search): void {
-                                            foreach ($value as $field) {
-                                                $query->orWhere($field, 'like', "%{$search}%");
-                                            }
-                                        }
-                                    );
-                                }
-                            );
+                            return $query->orWhere($value, 'like', "%{$search}%");
+                        }
+
+                        return $this->applyRelationSearchable($query, $key, $value, $search);
+                    }
+                );
+            }
+        );
+    }
+
+    protected function applyRelationSearchable($query, $relation, $fields, $search)
+    {
+        return $query->orWhereHas(
+            $relation,
+            function (Builder $query) use ($fields, $search): void {
+                $query->where(
+                    function (Builder $query) use ($fields, $search): void {
+                        foreach ($fields as $field) {
+                            $query->orWhere($field, 'like', "%{$search}%");
                         }
                     }
-                }
-            );
-        }//end if
-
-        return $this;
+                );
+            }
+        );
     }
 
     /**
@@ -71,6 +75,20 @@ trait WithSearchable
             );
 
         $results[$relation][] = $property;
+
+        return $results;
+    }
+
+    private function resolveNestedSearchable(array $searchable)
+    {
+        $results = [];
+        foreach ($searchable as $field) {
+            if (Str::contains($field, '.')) {
+                $results = $this->addNestedRelation($field, $results);
+            } else {
+                $results[] = $field;
+            }
+        }
 
         return $results;
     }
--- tests/BuilderTest.php
+++ tests/BuilderTest.php
@@ -11,35 +11,6 @@ use Zing\QueryBuilder\QueryBuilder;
 
 class BuilderTest extends TestCase
 {
-    public function testSearchable(): void
-    {
-        request()->merge(['search' => '1', 'a' => '2']);
-        $actual = QueryBuilder::fromBuilder(User::class, request())
-            ->searchable(['b', 'c'])
-            ->enableFilters('a')
-            ->toSql();
-        $expected = User::query()
-            ->when(
-                request()->input('search'),
-                function ($query, $search) {
-                    return $query->where(
-                        function ($query) use ($search) {
-                            return $query->orWhere('b', 'like', "%{$search}%")
-                                ->orWhere('c', 'like', "%{$search}%");
-                        }
-                    );
-                }
-            )
-            ->when(
-                request()->input('a'),
-                function ($query, $value) {
-                    return $query->where('a', $value);
-                }
-            )
-            ->toSql();
-        self::assertSame($expected, $actual);
-    }
-
     public function testExact(): void
     {
         request()->merge(['name' => '2']);
@@ -306,22 +277,6 @@ class BuilderTest extends TestCase
         request()->merge(['name' => $user->name]);
         $actual = QueryBuilder::fromBuilder(Order::class, request())
             ->enableFilters(Filter::partial('name', 'user.name'))
-            ->count();
-        self::assertSame(2, $actual);
-    }
-
-    public function testSearchableRelation(): void
-    {
-        factory(Order::class)->times(3)->create();
-        $user = factory(User::class)->create();
-        factory(Order::class)->times(2)->create(
-            [
-                'user_id' => $user->getKey(),
-            ]
-        );
-        request()->merge(['search' => $user->name]);
-        $actual = QueryBuilder::fromBuilder(Order::class, request())
-            ->searchable('user.name')
             ->count();
         self::assertSame(2, $actual);
     }
