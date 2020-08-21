d782a43ce78b53e00cb9cb003f0eccd19e22a0a4
zingimmick <zingimmick@outlook.com>
2020-08-21 18:21:02 +0800

    Improve coding standard config

--- rector.php
+++ rector.php
@@ -3,29 +3,13 @@
 declare(strict_types=1);
 
 use Rector\Core\Configuration\Option;
-use Rector\DeadCode\Rector\Function_\RemoveUnusedFunctionRector;
-use Rector\PHPStan\Rector\Cast\RecastingRemovalRector;
 use Rector\PHPUnit\Rector\Class_\AddSeeTestAnnotationRector;
-use Rector\Renaming\Rector\Class_\RenameClassRector;
 use Rector\Set\ValueObject\SetList;
+use Rector\SOLID\Rector\Class_\ChangeReadOnlyVariableWithDefaultValueToConstantRector;
 use Rector\SOLID\Rector\Class_\FinalizeClassesWithoutChildrenRector;
-use Rector\SOLID\Rector\Class_\RepeatedLiteralToClassConstantRector;
-use Rector\SOLID\Rector\ClassMethod\ChangeReadOnlyVariableWithDefaultValueToConstantRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $services = $containerConfigurator->services();
-    $services->set(RenameClassRector::class)
-        ->call(
-            'configure',
-            [
-                [
-                    RenameClassRector::OLD_TO_NEW_CLASSES => [
-                        'Zing\QueryBuilder\QueryBuilder' => \Zing\QueryBuilder\Builders\QueryBuilder::class,
-                    ],
-                ],
-            ]
-        );
     $parameters = $containerConfigurator->parameters();
     $parameters->set(
         Option::SETS,
@@ -48,20 +32,17 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         [
             FinalizeClassesWithoutChildrenRector::class,
             ChangeReadOnlyVariableWithDefaultValueToConstantRector::class,
-            RecastingRemovalRector::class,
-            RepeatedLiteralToClassConstantRector::class,
             AddSeeTestAnnotationRector::class,
-            RemoveUnusedFunctionRector::class,
         ]
     );
     $parameters->set(
         Option::PATHS,
         [
-            'config',
-            'src',
-            'tests',
-            'ecs.php',
-            'rector.php',
+            __DIR__ . '/config',
+            __DIR__ . '/src',
+            __DIR__ . '/tests',
+            __DIR__ . '/ecs.php',
+            __DIR__ . '/rector.php',
         ]
     );
 };
