4aafb825db86edbc5febf8c80ea41bfc8f7cc4d1
Zing <zingimmick@outlook.com>
2021-11-15 10:27:31 +0800

    Search with filters (#136)
    * Search with filters
    
    * Search with filters
    
    * Rector Rectify
    
    * Search with filters
    
    * Search with filters
    
    * Rector Rectify
    
    * Rector Rectify
    
    * Search with filters
    
    * Search with filters
    
    * Search with filters
    
    * Search with filters
    
    * Search with filters
    
    * Rector Rectify
    
    Co-authored-by: zingimmick <zingimmick@users.noreply.github.com>

--- src/Concerns/WithSearchable.php
+++ src/Concerns/WithSearchable.php
@@ -6,13 +6,15 @@ namespace Zing\QueryBuilder\Concerns;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Str;
+use Zing\QueryBuilder\Exceptions\ParameterException;
+use Zing\QueryBuilder\Filter;
 
 trait WithSearchable
 {
     use NestedRelation;
 
     /**
-     * @param string|array<string> $searchable
+     * @param string|\Zing\QueryBuilder\Filter|array<(string|\Zing\QueryBuilder\Filter)> $searchable
      *
      * @return $this
      */
@@ -35,7 +37,7 @@ trait WithSearchable
 
     /**
      * @param mixed $search
-     * @param array<int|string, string|array<string>> $searchable
+     * @param array<(int|string), (string|array<string>|\Zing\QueryBuilder\Filter)> $searchable
      *
      * @return $this
      */
@@ -45,6 +47,18 @@ trait WithSearchable
             function (Builder $query) use ($search, $searchable): void {
                 collect($searchable)->each(
                     function ($value, $key) use ($query, $search): void {
+                        if ($value instanceof Filter) {
+                            if ($value->getDefault() !== null) {
+                                throw ParameterException::unsupportedFilterWithDefaultValueForSearch();
+                            }
+
+                            $query->orWhere(function ($query) use ($value, $search): void {
+                                $value->filter($query, $search);
+                            });
+
+                            return;
+                        }
+
                         if (is_numeric($key)) {
                             $query->orWhere($value, 'like', sprintf('%%%s%%', $search));
 
@@ -81,15 +95,15 @@ trait WithSearchable
     }
 
     /**
-     * @param array<string> $searchable
+     * @param array<(string|\Zing\QueryBuilder\Filter)> $searchable
      *
-     * @return array<int|string, string|array<string>>
+     * @return array<(int|string), (string|array<string>|\Zing\QueryBuilder\Filter)>
      */
     private function resolveNestedSearchable(array $searchable)
     {
         $results = [];
         foreach ($searchable as $singleSearchable) {
-            if (Str::contains($singleSearchable, '.')) {
+            if (! $singleSearchable instanceof Filter && Str::contains($singleSearchable, '.')) {
                 [$relation, $property] = $this->resolveNestedRelation($singleSearchable);
 
                 $results[$relation][] = $property;
--- src/Exceptions/ParameterException.php
+++ src/Exceptions/ParameterException.php
@@ -6,6 +6,11 @@ namespace Zing\QueryBuilder\Exceptions;
 
 class ParameterException extends Exception
 {
+    public static function unsupportedFilterWithDefaultValueForSearch(): self
+    {
+        return new self('unsupported filter with default value for search');
+    }
+
     public static function tooFewElementsForBetweenExpression(): self
     {
         return new self('Too few elements for between expression, at least 2 elements expected');
