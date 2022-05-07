5cce6f267afe0ca5fe79b47f2b6e94a6d1dff2ae
zingimmick <zingimmick@outlook.com>
2022-05-07 19:13:02 +0800

    Add support for new ECSConfig and RectorConfig class

--- rector.php
+++ rector.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use Rector\Core\Configuration\Option;
 use Rector\Naming\Rector\Assign\RenameVariableToMatchMethodCallReturnTypeRector;
 use Rector\Naming\Rector\ClassMethod\RenameParamToMatchTypeRector;
 use Rector\PHPUnit\Rector\Class_\AddSeeTestAnnotationRector;
@@ -11,37 +10,26 @@ use Rector\Privatization\Rector\Class_\FinalizeClassesWithoutChildrenRector;
 use Rector\Privatization\Rector\Class_\RepeatedLiteralToClassConstantRector;
 use Rector\Privatization\Rector\MethodCall\PrivatizeLocalGetterToPropertyRector;
 use Rector\Set\ValueObject\LevelSetList;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Zing\CodingStandard\Set\RectorSetList;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(RectorSetList::CUSTOM);
-    $containerConfigurator->import(PHPUnitSetList::PHPUNIT_CODE_QUALITY);
-    $containerConfigurator->import(LevelSetList::UP_TO_PHP_73);
-
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(Option::PARALLEL, true);
-    $parameters->set(
-        Option::SKIP,
-        [
-            RenameVariableToMatchMethodCallReturnTypeRector::class,
-            RenameParamToMatchTypeRector::class,
-            AddSeeTestAnnotationRector::class,
-            FinalizeClassesWithoutChildrenRector::class,
-            RepeatedLiteralToClassConstantRector::class,
-            PrivatizeLocalGetterToPropertyRector::class,
-        ]
-    );
-    $parameters->set(
-        Option::PATHS,
-        [
-            __DIR__ . '/bin',
-            __DIR__ . '/samples',
-            __DIR__ . '/config',
-            __DIR__ . '/src',
-            __DIR__ . '/tests',
-            __DIR__ . '/ecs.php',
-            __DIR__ . '/rector.php',
-        ]
-    );
+return static function (Rector\Config\RectorConfig $rectorConfig): void {
+    $rectorConfig->sets([LevelSetList::UP_TO_PHP_73, PHPUnitSetList::PHPUNIT_CODE_QUALITY, RectorSetList::CUSTOM]);
+    $rectorConfig->parallel();
+    $rectorConfig->skip([
+        RenameVariableToMatchMethodCallReturnTypeRector::class,
+        RenameParamToMatchTypeRector::class,
+        AddSeeTestAnnotationRector::class,
+        FinalizeClassesWithoutChildrenRector::class,
+        RepeatedLiteralToClassConstantRector::class,
+        PrivatizeLocalGetterToPropertyRector::class,
+    ]);
+    $rectorConfig->paths([
+        __DIR__ . '/bin',
+        __DIR__ . '/samples',
+        __DIR__ . '/config',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+        __DIR__ . '/ecs.php',
+        __DIR__ . '/rector.php',
+    ]);
 };
