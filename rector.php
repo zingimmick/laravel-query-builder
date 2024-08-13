e02d9d4887cbcdcb1f74742ec512fb2b255e8415
Zing <zingimmick@outlook.com>
2024-08-14 00:11:21 +0800

    Remove rules that were never registered

--- rector.php
+++ rector.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 use Rector\Naming\Rector\Assign\RenameVariableToMatchMethodCallReturnTypeRector;
 use Rector\Naming\Rector\ClassMethod\RenameParamToMatchTypeRector;
-use Rector\PHPUnit\CodeQuality\Rector\Class_\AddSeeTestAnnotationRector;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Privatization\Rector\MethodCall\PrivatizeLocalGetterToPropertyRector;
 use Rector\Set\ValueObject\LevelSetList;
@@ -16,7 +15,6 @@ return static function (\Rector\Config\RectorConfig $rectorConfig): void {
     $rectorConfig->skip([
         RenameVariableToMatchMethodCallReturnTypeRector::class,
         RenameParamToMatchTypeRector::class,
-        AddSeeTestAnnotationRector::class,
         PrivatizeLocalGetterToPropertyRector::class,
     ]);
     $rectorConfig->paths([
