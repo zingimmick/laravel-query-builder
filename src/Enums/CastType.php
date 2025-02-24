8f72a2c7f542bdeec9a18486d2b3736d63cbf328
Zing <zingimmick@outlook.com>
2025-02-24 23:57:08 +0800

    Support for Laravel 12 (#244)

--- src/Enums/CastType.php
+++ src/Enums/CastType.php
@@ -6,28 +6,13 @@ namespace Zing\QueryBuilder\Enums;
 
 class CastType
 {
-    /**
-     * @var string
-     */
     public const ORIGINAL = 'original';
 
-    /**
-     * @var string
-     */
     public const STRING = 'string';
 
-    /**
-     * @var string
-     */
     public const BOOLEAN = 'boolean';
 
-    /**
-     * @var string
-     */
     public const ARRAY = 'array';
 
-    /**
-     * @var string
-     */
     public const INTEGER = 'integer';
 }
